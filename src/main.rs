4bde2cf5153c5eae9c2b30a0c43031bff267bb4a
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-20 08:59:10 -0700

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+
+fn vectors() {
+    let mut a = Vec::new();
+    a.push(1);
+    a.push(2);
+    a.push(3);
+
+    println!("a = {:?}", a);
+
+    a.push(44);
+    println!("a = {:?}", a);
+
+    let idx:usize = 0;
+
+    a[idx] = 321;
+    println!("a[0] = {}", a[idx]);
+
+    // Option
+    match a.get(6) {
+        Some(x) => println!("a[6] = {}", x),
+        None => println!("error, no such element")
+    }
+
+    for x in &a { println!("{}", x); }
+
+    a.push(77);
+    println!("{:?}", a);
+
+    let last_elem = a.pop();
+    println!("Last Elem is {:?}, a = {:?}", last_elem, a);
+
+    while let Some(x) = a.pop() {
+        println!("{}", x);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    vectors();
 }
