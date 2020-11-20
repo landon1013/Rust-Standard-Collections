8cbad4b6e0ea2ff88ec568b50768a812b7fdbc11
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-20 09:13:16 -0700

    Hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 fn vectors() {
     let mut a = Vec::new();
@@ -34,6 +35,33 @@ fn vectors() {
     }
 }
 
+fn hash_map() {
+    let mut shapes = HashMap::new();
+    shapes.insert(String::from("triangle"), 3);
+    shapes.insert(String::from("square"), 4);
+
+    println!("a square has {} sides", 
+        shapes["square".into()]);
+
+    for (key, value) in &shapes {
+        println!("{} : {}", key, value);
+    }
+
+    shapes.insert("square".into(), 5);
+
+    println!("{:?}", shapes);
+
+    shapes.entry("circle".into()).or_insert(1); {
+        let actual = shapes
+            .entry("circle".into())
+            .or_insert(2);
+        *actual = 0;
+    }
+
+    println!("{:?}", shapes);
+}
+
 fn main() {
-    vectors();
+    //vectors();
+    hash_map();
 }
