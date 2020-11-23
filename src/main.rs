8fd2b6fb84f4955a995fbe046dc606ba5a55da15
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-23 10:43:16 -0700

    Hashset

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 
+// Vectors
 fn vectors() {
     let mut a = Vec::new();
     a.push(1);
@@ -35,6 +37,7 @@ fn vectors() {
     }
 }
 
+// HashMap
 fn hash_map() {
     let mut shapes = HashMap::new();
     shapes.insert(String::from("triangle"), 3);
@@ -61,7 +64,62 @@ fn hash_map() {
     println!("{:?}", shapes);
 }
 
+// HashSet
+fn hash_set() {
+    let mut greeks = HashSet::new();
+    greeks.insert("gamma");
+    greeks.insert("delta");
+    println!("{:?}", greeks);
+
+    greeks.insert("delta");
+    println!("{:?}", greeks);
+
+    let added_vega = greeks.insert("vega");
+    if added_vega {
+        println!("We added Vega!");
+    }
+
+    println!("{:?}", greeks);
+
+    if !greeks.contains("kappa") {
+        println!("We don't have kappa");
+    }
+
+    let removed = greeks.remove("delta");
+
+    if removed {
+        println!("We removed delta");
+    }
+
+    println!("{:?}", greeks);
+
+    let _1_5: HashSet<_> = (1..=5).collect();
+    let _6_10: HashSet<_> = (6..=10).collect();
+    let _1_10: HashSet<_> = (1..=10).collect();
+    let _2_8: HashSet<_> = (2..=8).collect();
+
+    println!(
+        "is {:?} a subset of {:?}? {}",
+        _2_8, _1_10,
+        _2_8.is_subset(&_1_10)
+    );
+
+    println!(
+        "is {:?} a disjoint of {:?}? {}",
+        _1_5, _6_10,
+        _1_5.is_disjoint(&_6_10)
+    );
+
+    println!(
+        "items in either {:?} and {:?} are {:?}",
+        _2_8,
+        _6_10,
+        _2_8.union(&_6_10)
+    );
+}
+
 fn main() {
     //vectors();
-    hash_map();
+    //hash_map();
+    hash_set();
 }
